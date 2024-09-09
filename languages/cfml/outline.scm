40eac4cb155c9929fc0c2ea3ceb63735b81c3a32
Gareth Eddies <ghedwards@gmail.com>
2024-09-09 18:24:03 +1000

    Create outline for basic tag functions

---
+++ languages/cfml/outline.scm
@@ -0,0 +1,8 @@
+(cf_function 
+	(cf_attribute
+    	(cf_attribute_name) @attribute (#eq? @attribute "name")
+        (quoted_cf_attribute_value 
+        	(attribute_value) @name
+        )
+	)
+) @item
