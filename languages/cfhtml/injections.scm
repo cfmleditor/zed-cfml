e81f1e334283a6a47611c0591ed22a293439d986
Gareth Edwards <ghedwards@gmail.com>
2026-03-15 22:21:40 +1000

    Fix for cfscript injection

--- languages/cfhtml/injections.scm
+++ languages/cfhtml/injections.scm
@@ -9,4 +9,5 @@
 ((cf_script_tag
   (cf_script_content) @injection.content)
  (#set! injection.language "cfscript"))
-((cf_script_content) @injection.content (#set! injection.language "cfscript")) 
+ 
+((cf_script_content) @injection.content (#set! injection.language "CFML (Script)")) 
